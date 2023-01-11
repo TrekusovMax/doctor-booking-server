d865ac13ae4433b6934f6f5db1f6bd2edfba0b7e
Maxim <maxtrekusovsite@gmail.com>
2023-01-11 23:28:57 +0300

    deploy

--- services/order.service.js
+++ services/order.service.js
@@ -5,17 +5,20 @@ class OrderService {
   crypt(payload) {
     // generate 16 bytes of random data
 
-    const initVector = Buffer.from(process.env.INIT_VECTOR, 'hex')
+    //const initVector = Buffer.from(process.env.INIT_VECTOR, 'hex')
+    const initVector = Buffer.from(config.get('initVector'), 'hex')
 
     // protected data
     const message = JSON.stringify(payload)
 
     // secret key generate 32 bytes of random data
-    const Securitykey = Buffer.from(process.env.SECURITY_KEY, 'hex')
+    //const Securitykey = Buffer.from(process.env.SECURITY_KEY, 'hex')
+    const Securitykey = Buffer.from(config.get('Securitykey'), 'hex')
 
     // the cipher function
     const cipher = crypto.createCipheriv(
-      process.env.ALGORITHM,
+      //process.env.ALGORITHM,
+      config.get('algorithm'),
       Securitykey,
       initVector,
     )
@@ -24,10 +27,13 @@ class OrderService {
     return encryptedData
   }
   decrypt(payload) {
-    const Securitykey = Buffer.from(process.env.SECURITY_KEY, 'hex')
-    const initVector = Buffer.from(process.env.INIT_VECTOR, 'hex')
+    /*     const Securitykey = Buffer.from(process.env.SECURITY_KEY, 'hex')
+    const initVector = Buffer.from(process.env.INIT_VECTOR, 'hex') */
+    const Securitykey = Buffer.from(config.get('Securitykey'), 'hex')
+    const initVector = Buffer.from(config.get('initVector'), 'hex')
     const decipher = crypto.createDecipheriv(
-      process.env.ALGORITHM,
+      //process.env.ALGORITHM,
+      config.get('algorithm'),
       Securitykey,
       initVector,
     )
